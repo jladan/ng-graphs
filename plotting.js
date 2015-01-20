d69c0e2f53efde2e89543e20914757a3e726d15d
John Ladan <john@ladan.ca>
2015-01-20 10:24:14 -0500

    Most of the way to one solution

--- plotting.js
+++ plotting.js
@@ -1,29 +1,74 @@
 angular.module('plotting', [])
 
 .factory('d3Service', function () {
+    // TODO: determine a good way of doing this
     var d3;
     return d3;
 })
 .directive('figure', ['d3Service', function (d3Service) {
+    var d3 = window.d3;
 
     return {
         restrict: 'EA',
-        scope: {},
+        scope: {
+            width: '=',
+            height: '=',
+        },
         controller: function($scope) {
-            // This will likely need to be created in the link function
-            $scope.svgElement;
-
-            /* The following functions are to add things to the figure
-             * such as paths, lines, circles, etc.
+            /* add a curve to the svg plot
+             * plotData has to be an array of pairs [[x1,y1],[x2,y2]...]
              */
-            this.addAxes = function(path) {
-                // Script to add path to SVG here
+            this.addPath = function(plotData) {
+                // Define the path generator with current scales
+                var pathGen = d3.svg.line()
+                    .x(function(d) {return $scope.xScale(d[0]); })
+                    .y(function(d) {return $scope.yScale(d[1]); })
+                    .interpolate("linear");
+
+                // Now, the plot is actually added to the svg
+                var plot = $scope.svg.append("path")
+                    .attr("d", pathGen(plotData))
+                    .attr("stroke", "blue")
+                    .attr("stroke-width", 2)
+                    .attr("fill", "none");
             };
 
-            this.addPath = function(path) {
-                // Script to add path to SVG here
+            this.addAxes = function(xDomain, yDomain) {
+                // set up the scales
+                $scope.xScale = d3.scale.linear()
+                    .domain(xDomain).range(scope.xRange);
+                $scope.xScale = d3.scale.linear()
+                    .domain(xDomain).range(scope.xRange);
+
+                // create the axis objects
+                var xAxis = d3.svg.axis()
+                    .scale($scope.xScale)
+                    .ticks(5)
+                    .orient("bottom");
+                var yAxis = d3.svg.axis()
+                    .scale($scope.yScale)
+                    .ticks(5)
+                    .orient("left");
+                // Draw the axes
+                $scope.svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + (h/2) + ")")
+                    .call(xAxis);
+                $scope.svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(" + w/2 + ",0)")
+                    .call(yAxis);
+
             };
         },
+        link: function(scope, elm, attrs) {
+            // Create the svg area that we'll draw in
+            var scope.svg = d3.select(elm[0])
+                    .append("svg")
+                    .attr("height", scope.height)
+                    .attr("width", scope.width)
+
+
         templateUrl: '/static/scripts/courseware/templates/uw-slide.html'
     };
 }])
@@ -36,7 +81,7 @@ angular.module('plotting', [])
             yRange: '='
         },
         link: function(scope, element, attrs, figureCtrl) {
-            figureCtrl.addPath(scope);
+            figureCtrl.addAxes(xDomain, yDomain);
         },
         templateUrl: '/static/scripts/courseware/templates/uw-slide.html'
     };
